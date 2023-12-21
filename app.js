6fca429731dfca459dd4dc39e9981cf0c44d9723
Nimesh Kumar <nimeshkumarteacher@gmail.com>
2023-12-21 19:50:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ async function fetchData() {
         this.firstChild.style="border: 1px solid #4E9F3D;";
         current_crop=this.textContent.trim();
         console.log(current_crop);
-        if (current_crop==="Wheat" || current_crop==="गेहूँ"){
+        if (current_crop==="Wheat"){
             let crop_stages_ul=document.querySelector("#crop_stages_ul");
             crop_stages_ul.innerHTML="";
             if (getCurrentPage() === "index.html")
@@ -160,7 +160,7 @@ async function fetchData() {
                   console.log(crop_stage);
               });
         }
-        else if (current_crop==="Jowar" || current_crop==="ज्वार"){
+        else if (current_crop==="Jowar"){
             
             let crop_stages_ul=document.querySelector("#crop_stages_ul");
             crop_stages_ul.innerHTML="";
