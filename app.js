489e23440a03d790ac1c5c38745e4f656954ee7c
Nimesh Kumar <nimeshkumarteacher@gmail.com>
2023-12-22 10:01:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ function calc_water_need(){
               console.log("volume_of_field",volume_of_field);
               volume_req=moisture_required * volume_of_field * 1000;
                     console.log("volume_req",volume_req);
-              hours_req=(volume_req/4500);
+              hours_req=(volume_req/45000);
               console.log("hours_req",hours_req);
               return hours_req;
                     }
