42c3e4e538d1b5c155d5159904a9df95fc976553
Nimesh Kumar <nimeshkumarteacher@gmail.com>
2023-12-22 09:40:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function calc_water_need(){
                     
               let index_of_stage=jowar_stage_eng.indexOf(current_stage);
                     
-              volume_of_field= 2.47105 * data_sheet_jowar[index_of_stage];  //2.47105 is land size
+              volume_of_field= 4000 * data_sheet_jowar[index_of_stage];  //2.47105 is land size
               console.log("volume_of_field",volume_of_field);
               volume_req=moisture_required * volume_of_field;
                     console.log("volume_req",volume_req);
